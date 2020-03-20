6c94ca23ad30e0880e16a49baae94e3e973cebac
ekaynac <47036420+ekaynac@users.noreply.github.com>
2020-03-20 18:25:21 +0300

    Create EnesKaynakci(2-5).py

---
+++ EnesKaynakci(2-5).py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 17:55:56 2020
+
+@author: enes_
+"""
+
+
+with open("yasamayadair.txt", "r", encoding='utf8') as f:
+    lines = f.readlines()
+    
+    
+    
+print("Şiirin dize sayısı = {}".format(len(lines)))
+
+first_word = []
+
+for x in lines:
+    x=list(x)
+    for i in x:
+        i= list(i)
+        if (i != " "):
+            first_word[x].appent(i)
+        else:
+            break
+            
